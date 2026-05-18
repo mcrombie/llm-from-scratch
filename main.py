96ca1f64ccf5e110953e367cb969c5c82b7491e2
Michael Crombie <mcrombie1994@gmail.com>
2026-05-18 10:40:59 -0400

    4.7 Generating text
    Using the model to generate gibberish text because it still needs to be trained.

--- main.py
+++ main.py
@@ -290,6 +290,20 @@ class GPTModel(nn.Module):
         logits = self.out_head(x)
         return logits 
     
+
+def generate_text_simple(model, idx, 
+                         max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+            
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
         raw_text = f.read()
@@ -661,6 +675,24 @@ def main():
     total_size_bytes = total_params * 4
     total_size_mb = total_size_bytes / (1024 * 1024)
     print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+    start_context = "Hello, I am" 
+    encoded = tokenizer.encode(start_context) 
+    print("encoded:", encoded) 
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    print("encoded_tensor.shape:", encoded_tensor.shape)
+
+    model.eval()
+    out = generate_text_simple(
+        model=model,
+        idx=encoded_tensor, 
+        max_new_tokens=6, 
+        context_size=GPT_CONFIG_124M["context_length"] ) 
+    print("Output:", out) 
+    print("Output length:", len(out[0]))
+
+    decoded_text = tokenizer.decode(out.squeeze(0).tolist()) 
+    print(decoded_text)
 
 if __name__ == "__main__":
     main()
