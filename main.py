29fcbd96d74529b4de92b812fc49faa29048024f
Michael Crombie <mcrombie1994@gmail.com>
2026-05-22 07:53:03 -0400

    5.2 Training an LLM
    First attempt at actual training :)

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ if not os.path.exists(FILE_PATH):
 import torch.nn as nn 
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 class SelfAttention_v1(nn.Module):
     def __init__(self, d_in, d_out):
@@ -338,6 +339,77 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         else: 
             break 
     return total_loss / num_batches
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter): 
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train() 
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context): 
+    model.eval() 
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad(): 
+        token_ids = generate_text_simple(
+            model=model, idx=encoded, max_new_tokens=50, context_size=context_size
+            )
+    decoded_text = token_ids_to_text(token_ids, tokenizer) 
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+
+def train_model_simple(model, 
+                       train_loader, 
+                       val_loader, 
+                       optimizer, 
+                       device, 
+                       num_epochs, 
+                       eval_freq, 
+                       eval_iter, 
+                       start_context, 
+                       tokenizer): 
+                       
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1 
+    for epoch in range(num_epochs):
+        model.train() 
+        for input_batch, target_batch in train_loader: 
+            optimizer.zero_grad() 
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1 
+            
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}"
+                      ) 
+        generate_and_print_sample(model, tokenizer, device, start_context) 
+    return train_losses, val_losses, track_tokens_seen
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3)) 
+    ax1.plot(epochs_seen, train_losses, label="Training loss") 
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True)) 
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
+    
 
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
@@ -833,6 +905,27 @@ def main():
         val_loss = calc_loss_loader(val_loader, model, device) 
     print("Training loss:", train_loss) 
     print("Validation loss:", val_loss)
+
+    torch.manual_seed(123) 
+    model = GPTModel(GPT_CONFIG_124M) 
+    model.to(device) 
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1 ) 
+    num_epochs = 10 
+    train_losses, val_losses, tokens_seen = train_model_simple(model, 
+                                                               train_loader, 
+                                                               val_loader, 
+                                                               optimizer, 
+                                                               device, 
+                                                               num_epochs=num_epochs, 
+                                                               eval_freq=5, 
+                                                               eval_iter=5, 
+                                                               start_context="Every effort moves you", 
+                                                               tokenizer=tokenizer)
+    
+
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses)) 
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
     
 if __name__ == "__main__":
     main()
