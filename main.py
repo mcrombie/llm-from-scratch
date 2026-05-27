33b50e6e0cc7f3591efc73ca8d1fb2683786c73d
Michael Crombie <mcrombie1994@gmail.com>
2026-05-27 06:52:55 -0400

    5.3.3 Modifying the text generation function
    Generating text

--- main.py
+++ main.py
@@ -908,25 +908,25 @@ def main():
 
 
     # TRAINING THE MODEL
-    # torch.manual_seed(123) 
-    # model = GPTModel(GPT_CONFIG_124M) 
-    # model.to(device) 
-    # optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1 ) 
-    # num_epochs = 10 
-    # train_losses, val_losses, tokens_seen = train_model_simple(model, 
-    #                                                            train_loader, 
-    #                                                            val_loader, 
-    #                                                            optimizer, 
-    #                                                            device, 
-    #                                                            num_epochs=num_epochs, 
-    #                                                            eval_freq=5, 
-    #                                                            eval_iter=5, 
-    #                                                            start_context="Every effort moves you", 
-    #                                                            tokenizer=tokenizer)
+    torch.manual_seed(123) 
+    model = GPTModel(GPT_CONFIG_124M) 
+    model.to(device) 
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1 ) 
+    num_epochs = 10 
+    train_losses, val_losses, tokens_seen = train_model_simple(model, 
+                                                               train_loader, 
+                                                               val_loader, 
+                                                               optimizer, 
+                                                               device, 
+                                                               num_epochs=num_epochs, 
+                                                               eval_freq=5, 
+                                                               eval_iter=5, 
+                                                               start_context="Every effort moves you", 
+                                                               tokenizer=tokenizer)
     
 
-    # epochs_tensor = torch.linspace(0, num_epochs, len(train_losses)) 
-    # plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses)) 
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
 
 
     # transferring the model back from the GPU to the CPU since inference with a relatively small model does not require a GPU.
@@ -979,6 +979,31 @@ def main():
     print(new_logits)
     topk_probas = torch.softmax(new_logits, dim=0)
     print(topk_probas)
+
+    def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:,-context_size:]
+            with torch.no_grad():
+                logits = model(idx_cond)
+            logits = logits[:,-1,:]
+            if top_k is not None:
+                top_logits,_ = torch.topk(logits, top_k)
+                min_val = top_logits[:,-1]
+                logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+            if temperature > 0.0:
+                logits = logits / temperature
+                probs = torch.softmax(logits,dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+            if idx_next == eos_id:
+                break
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+    
+    torch.manual_seed(123) 
+    token_ids = generate(model=model, idx=text_to_token_ids("Every effort moves you", tokenizer), max_new_tokens=15, context_size=GPT_CONFIG_124M["context_length"], top_k=25, temperature=1.4 ) 
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
 
 
 if __name__ == "__main__":
