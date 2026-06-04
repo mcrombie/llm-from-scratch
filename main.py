c28492f4fe19dfb9ddc3e88cfad75f2aa38beea4
Michael Crombie <mcrombie1994@gmail.com>
2026-06-04 08:25:21 -0400

    6.3 Creating data loaders
    To load data, we batch messages  by padding them to the length of the longest message in the dataset or batch

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if hasattr(sys.stdout, "reconfigure"):
 
 import tiktoken
 import torch
+from torch.utils.data import Dataset, DataLoader
 
 from tokenizers import SimpleTokenizerV2
 from dataset import create_dataloader_v1
@@ -310,6 +311,34 @@ class GPTModel(nn.Module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         return logits 
+    
+class SpamDataset(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=0):
+        self.data = pd.read_csv(csv_file)
+        self.encoded_texts = [tokenizer.encode(text) for text in self.data["Text"]]
+        if max_length is None:
+            self.max_length = self.longest_encoded_length()
+        else:
+            self.max_length = max_length
+            self.encoded_texts = [encoded_text[:self.max_length] for encoded_text in self.encoded_texts]
+        
+        self.encoded_texts = [encoded + [pad_token_id] * (self.max_length - len(encoded)) for encoded in self.encoded_texts]
+    
+    def __getitem__(self, index):
+        encoded = self.encoded_texts[index]
+        label = self.data.iloc[index]["Label"]
+        return (torch.tensor(encoded, dtype=torch.long), torch.tensor(label, dtype=torch.long))
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def longest_encoded_length(self):
+        max_length = 0
+        for encoded_text in self.encoded_texts:
+            encoded_length = len(encoded_text)
+            if len(encoded_text) > max_length:
+                max_length = encoded_length
+        return max(len(encoded) for encoded in self.encoded_texts)
     
 
 def generate_text_simple(model, idx, 
@@ -1195,6 +1224,32 @@ def main():
     train_df.to_csv("train.csv", index=None)
     validation_df.to_csv("validation.csv", index=None)
     test_df.to_csv("test.csv", index=None)
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    print(tokenizer.encode("<endoftext>", allowed_special={"<endoftext>"}))
+
+    train_dataset = SpamDataset(csv_file="train.csv", tokenizer=tokenizer, max_length=None)
+
+    print(train_dataset.max_length)
+
+    val_dataset = SpamDataset(csv_file="validation.csv", tokenizer=tokenizer, max_length=train_dataset.max_length)
+    test_dataset = SpamDataset(csv_file="test.csv", tokenizer=tokenizer, max_length=train_dataset.max_length)
+
+    num_workers = 0
+    batch_size = 8
+    torch.manual_seed(123)
+
+    train_loader = DataLoader(dataset = train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+    val_loader = DataLoader(dataset = val_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=False)
+    test_loader = DataLoader(dataset = test_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=False)
+
+    for input_batch, target_batch in train_loader:
+        print("Input batch dimensions:", input_batch.shape)
+        print("Target batch dimensions:", target_batch.shape)
+
+    print(f"{len(train_loader)} training batches")
+    print(f"{len(val_loader)} validation batches")
+    print(f"{len(test_loader)} test batches")
 
 if __name__ == "__main__":
     main()
