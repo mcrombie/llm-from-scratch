b2c2f4f3e43502edfd5ced2f41745d5ed8eaa885
Michael Crombie <mcrombie1994@gmail.com>
2026-06-17 10:14:15 -0400

    6.8 Using the LLM as a spam classifier
    Function to classify spam after fine tuning is complete

--- main.py
+++ main.py
@@ -570,6 +570,27 @@ def plot_values(epochs_seen, examples_seen, train_values, val_values, label="los
     plt.savefig(f"{label}-plot.pdf")
     plt.show()
 
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
+    model.eval()
+    
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[0]
+    if max_length is None:
+        max_length = supported_context_length
+    max_length = min(max_length, supported_context_length)
+
+    input_ids = input_ids[:max_length]
+
+    input_ids += [pad_token_id] * (max_length - len(input_ids)) 
+
+    input_tensor = torch.tensor([input_ids], device=device)
+
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]
+    predicted_label = torch.argmax(logits, dim=-1).item()
+
+    return "spam" if predicted_label == 1 else "not spam"
+
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
         raw_text = f.read()
@@ -1497,6 +1518,24 @@ def main():
     print(f"Training accuracy: {train_accuracy*100:.2f}%")
     print(f"Validation accuracy: {val_accuracy*100:.2f}%")
     print(f"Test accuracy: {test_accuracy*100:.2f}%")
+
+    text_1 = ("You are a winner you have been specially "
+              "selected to receive $1000 cash or a $2000 award.")
+    print(classify_review(text_1, model, tokenizer, device, max_length=train_dataset.max_length))
+
+    text_2 = (
+        "Hey, just wanted to check if we're still on "
+        "for dinner tonight? Let me know!"
+    )
+
+    print(classify_review(text_2, model, tokenizer, device, max_length=train_dataset.max_length))
+
+    torch.save(model.state_dict(), "review_classifier.pth")
+
+    model_state_dict = torch.load("review_classifier.pth", map_location=device)
+    model.load_state_dict(model_state_dict)
+
+
 
 if __name__ == "__main__":
     main()
