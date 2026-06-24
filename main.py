3d3ffa539726a8635faa53f40b8d613732105767
Michael Crombie <mcrombie1994@gmail.com>
2026-06-24 09:54:06 -0400

    7.3 Organizing data into training batches
    Custom collate function. Collate functions take a list of individual data samples and merge them into a single batch that can be processed efficiently by the model during training.

--- main.py
+++ main.py
@@ -345,6 +345,23 @@ class SpamDataset(Dataset):
             if len(encoded_text) > max_length:
                 max_length = encoded_length
         return max(len(encoded) for encoded in self.encoded_texts)
+
+
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.encoded_texts = []
+        for entry in data:
+            instruction_plus_input = format_input(entry)
+            response_text = f"\n\n### Response:\n{entry['output']}"
+            full_text = instruction_plus_input + response_text
+            self.encoded_texts.append(tokenizer.encode(full_text))
+
+    def __getitem__(self, index):
+        return self.encoded_texts[index]
+    
+    def __len__(self):
+        return len(self.data)
     
 
 def generate_text_simple(model, idx, 
@@ -613,6 +630,74 @@ def format_input(entry):
         f"\n\n### Input:\n{entry['input']}" if entry['input'] else ""
     )
     return instruction_text + input_text
+
+def custom_collate_draft_1(batch, pad_token_id=50256, device="cpu"):
+    batch_max_length = max(len(item) +1 for item in batch)
+    inputs_lst = []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        inputs_lst.append(inputs)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    return inputs_tensor
+
+def custom_collate_draft_2(batch, pad_token_id=50256, device="cpu"):
+    batch_max_length = max(len(item) +1 for item in batch)
+    inputs_lst, targets_lst = [],[]
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+def custom_collate_fn(batch, pad_token_id=50256, ignore_index=-100, allowed_max_length=None, device="cpu"):
+    batch_max_length = max(len(item) +1 for item in batch)
+    inputs_lst, targets_lst = [],[]
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
 
 def main():
     with open(FILE_PATH, "r", encoding="utf-8") as f:
@@ -1590,6 +1675,39 @@ def main():
     print("Training set length:", len(train_data))
     print("Validation set length:", len(val_data))
     print("Test set length:", len(test_data))
+
+    tokenizer  = tiktoken.get_encoding("gpt2")
+    print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
+
+    inputs_1 = [0,1,2,3,4]
+    inputs_2 = [5,6]
+    inputs_3 = [7,8,9]
+    batch = (inputs_1, inputs_2, inputs_3)
+    print(custom_collate_draft_1(batch))
+
+    inputs, targets = custom_collate_draft_2(batch)
+    print(inputs)
+    print(targets)
+
+
+    inputs, targets = custom_collate_fn(batch)
+    print(inputs)
+    print(targets)
+
+    logits_1 = torch.tensor([[-1.0, 1.0], [-0.5, 1.5]])
+    targets_1 = torch.tensor([0,1])
+    loss_1 = torch.nn.functional.cross_entropy(logits_1, targets_1)
+    print(loss_1)
+
+    logits_2 = torch.tensor([[-1.0, 1.0], [-0.5, 1.5], [-0.5, 1.5]])
+    targets_2 = torch.tensor([0,1,1])
+    loss_2 = torch.nn.functional.cross_entropy(logits_2, targets_2)
+    print(loss_2)
+
+    targets_3 = torch.tensor([0,1,-100])
+    loss_3 = torch.nn.functional.cross_entropy(logits_2, targets_3)
+    print(loss_3)
+    print("loss_1 == loss_3:", loss_1 == loss_3)
 
 
 
