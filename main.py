ef0868680b537ef17ce93213320ba24080c62b69
Michael Crombie <mcrombie1994@gmail.com>
2026-07-08 12:30:55 -0400

    7.8 Evaluating the fine-tuned LLM
    Using the Ollama application to automatically score the model's responses

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ from tqdm import tqdm
 
 import re
 
+import psutil
+
 
 
 URL = (
@@ -1866,6 +1868,105 @@ def main():
     file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL)}-sft.pth"
     torch.save(model.state_dict(), file_name)
     print(f"Model saved as {file_name}")
+
+    def check_if_running(process_name):
+        running = False
+        for proc in psutil.process_iter(['name']):
+            if process_name in proc.info['name']:
+                running = True
+                break
+        return running
+    
+    olamma_running = check_if_running("olamma")
+
+    if not olamma_running:
+        raise RuntimeError(
+            "The olamma server is not running. Please start the server before running this script."
+        )
+    print("Olamma running:", check_if_running("olamma"))
+
+    file_path = "instruction-data-with-response.json"
+    with open(file_path, "r") as file:
+        test_data = json.load(file)
+
+    def format_input(entry):
+        instruction_text = (
+            f"Below is an instruction that describes a task."
+            f"Write a response that appropriately completes the request."
+            f"\n\n### Instruction:\n{entry['instruction']}"
+        )
+
+        input_text = (
+            f"\n\n### Input:\n{entry['input']}" if entry['input'] else ""
+        )
+        return instruction_text + input_text
+    
+    # Querying a local ollama model
+    def query_model(prompt, model="llama3", url="http://localhost:11434/api/chat"):
+        data = {
+            "model": model,
+            "messages": [{"role": "user", "content": prompt}],
+            "options": {"seed": 123, "temperature": 0, "num_ctx": 2048}
+            }
+        
+        payload = json.dumps(data).encode("utf-8")
+        request = urllib.request.Request(url, data=payload, method="POST")
+
+        request.add_header("Content-Type", "application/json")
+
+        response_data = ""
+        with urllib.request.urlopen(request) as response:
+            while True:
+                line = response.readline().decode("utf-8")
+                if not line:
+                    break
+                response_json = json.loads(line)
+                response_data += response_json["message"]["content"]
+        return response_data
+    
+    model = "llama3"
+    result = query_model(prompt="What do Llamas eat?", model=model)
+    print(result)
+        
+
+    for entry in test_data[:3]:
+        prompt = (
+            f"Given the input `{format_input(entry)}`"
+            f"and correct output `{entry['output']}`,"
+            f"score the model response `{entry['model_response']}`"
+            f"on a scale from 0 to 100, where 100 is the best score."
+        )
+        print("\nDataset response:")
+        print(">>", entry["output"])
+        print("\nModel response:")
+        print(">>", entry["model_response"])
+        print("\nScore:")
+        print(">>", query_model(prompt=prompt, model=model))
+        print("\n----------------------")
+
+    
+    def generate_model_scores(json_data, json_key, model="llama3"):
+        scores = []
+        for entry in tdqm(json_data, desc="Scoring entries"):
+            prompt = (
+                f"Given the input `{format_input(entry)}`"
+                f"and correct output `{entry['output']}`,"
+                f"score the model response `{entry[json_key]}`"
+                f"on a scale from 0 to 100, where 100 is the best score."
+                f"Respond with the integer number only."
+            )
+            score = query_model(prompt=prompt, model=model)
+            try:
+                scores.append(int(score))
+            except ValueError:
+                print(f"Could not convert score '{score}' to int for entry: {entry}")
+                continue
+
+        return scores
+    
+    scores = generate_model_scores(test_data, json_key="model_response", model=model)
+    print(f"Number of scores: {len(scores)} of {len(test_data)}")
+    print(f"Average score: {sum(scores)/len(scores):.2f}\n")
                           
 
 if __name__ == "__main__":
